8afe504609c3c85d7e525b27817c8161af7660b3
Nataliia <archifabod@gmail.com>
2026-04-17 18:56:39 +0200

    Fix jvmTarget syntax and sync versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,7 +19,7 @@ subprojects {
     // Принудительно ставим 21 для всех подпроектов
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
-            jvmTarget = "1.8"
+            jvmTarget = "1_8"
         }
     }
 
